921b3908432ee765e02822e947049ad9650fe245
abhishek-g-joshi <ajabhishekgjoshi@gmail.com>
2021-02-22 23:32:12 +0530

    working on displaying errors

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 const User = require("../models/Users");
 const dotenv = require('dotenv').config();
-
+const validateSignUpInputs = require("../validation/signup");
+const validateSignInputs = require("../validation/signin");
 const secreteKey = process.env.SECRETE_KEY;
 
 const requireAuth = (req,res,next) => {
@@ -50,4 +51,17 @@ const checkUser = (req,res,next) => {
 
 }
 
-module.exports = { requireAuth, checkUser };
+// check errors
+const checkErrors = (req,res,next)=>{
+    const errors = validateSignInputs(req.body);
+    if(errors)
+    {
+        res.locals.errors = errors;
+        next();
+    }else{
+        res.locals.errors = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser, checkErrors };
