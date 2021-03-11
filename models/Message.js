e4890fe12c14fe0b4a6d926d04ceb02db7d83f41
Rohit Ompalsingh Pal <rohitopal23@gmail.com>
2021-03-11 20:43:54 +0530

    schema ready for message and discussion, testing pending

---
+++ models/Message.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+
+var messageSchema = new mongoose.Schema({
+
+    userName :{
+        type : String,
+        required : true
+    },
+    Datetime :{
+        type: Date,
+         required: true
+    },
+    content :{
+        type: String,
+        required: false
+    }
+});
+
+module.exports = mongoose.model("Message", messageSchema);
